f60ca039d5137febffd6ed02c1410dc9a99edbb8
dazzaXx <48131734+dazzaXx@users.noreply.github.com>
2025-12-29 22:33:21 +0000

    Revert "Add configurable circular deadzone for analog sticks per player"

--- src/openjvs.c
+++ src/openjvs.c
@@ -101,7 +101,7 @@ int main(int argc, char **argv)
         io.chainedIO = NULL;
 
         debug(1, "Init inputs\n");
-        JVSInputStatus inputStatus = initInputs(config.defaultGamePath, config.capabilitiesPath, config.secondCapabilitiesPath, &io, config.autoControllerDetection, config.deadzoneP1, config.deadzoneP2);
+        JVSInputStatus inputStatus = initInputs(config.defaultGamePath, config.capabilitiesPath, config.secondCapabilitiesPath, &io, config.autoControllerDetection);
 
         // Only report these errors if the status has changed
         // from the last run. Since we restart this thread every 200ms
